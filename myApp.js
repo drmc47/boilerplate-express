40888e88d93709da7b2653a15de586e6926eda19
drmc47 <>
2021-07-15 04:34:29 +0000

    app.use

--- myApp.js
+++ myApp.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 
+app.get('/', (req, res) => {
+  // res.send('Hello Express'); 
+  app.use('/public', express.static(__dirname + '/public'));
+  res.sendFile(__dirname + '/views/index.html')
+  
+})
+
+//console.log('Hello World')
+
 
 
 
