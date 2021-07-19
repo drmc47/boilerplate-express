fd37a5d2af869f4570f9b60c8234b4142591aa94
drmc47 <>
2021-07-19 20:22:38 +0000

    update 19/07

--- myApp.js
+++ myApp.js
@@ -2,47 +2,32 @@ var express = require('express');
 var app = express();
 
 
+const mySecret = process.env['MESSAGE_STYLE']
+app.use('/public', express.static(__dirname + '/public'));
+pruebaMiddleware = (req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+
+}
+
+app.use(pruebaMiddleware);
+
 app.get('/', (req, res) => {
   // res.send('Hello Express'); 
-  app.use('/public', express.static(__dirname + '/public'));
+  
   res.sendFile(__dirname + '/views/index.html')
   
 })
 
+app.get('/json', (req, res) => {
+
+  if (process.env.MESSAGE_STYLE == 'uppercase') {
+  res.json({message : 'HELLO JSON'})
+  }
+  else {
+    res.json({message : 'Hello json'})
+  }
+})
 //console.log('Hello World')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
